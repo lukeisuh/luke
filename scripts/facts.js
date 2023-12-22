842d267125d2c0fa2b2befbd18c00b5685ae6d35
Luke <BoganGuy@proton.me>
2023-12-23 06:19:27 +1100

    aa

--- scripts/facts.js
+++ scripts/facts.js
@@ -1,12 +1,12 @@
 const funFacts = [
-    "The Queen died on my birthday in 2022.",
-    "I almost drowned when I was 4.",
-    "I have over 3k hours in Fortnite.",
-    "I almost got stabbed when I was 11.",
-    "I used to play VRChat (I was a loser).",
-    "As of writing this, I am 63% through the GTA V campaign.",
-    "I made a cringe fediverse instance that got 4k users in a week (it ended badly).",
-    "Despite being bi, I still can't pull any bitches.",
+    "Fact 1",
+    "Fact 2",
+    "Fact 3",
+    "Fact 4",
+    "Fact 5",
+    "Fact 6",
+    "Fact 7",
+    "Fact 8",
     
 ];
 
